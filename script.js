33970378ba290ee6dc73218b2bfbbbfc00274a08
EvelynS-Dias <Evydesouza23150@gmail.com>
2025-03-06 10:32:41 -0300

    :sparkle: feat: criação da função verificarGanho e fimDeJogo

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
 
 class Player {
     id = 0
+    player = this.player;
 
- create_player (player) { //Aqui vai estar quando ele clicar num espaço vazio
-    if(player.getAttribute = "Player-X" ) {
-    }
-    else {
-
-    }
+ choose_player () { //Aqui vou fazer ele poder escolher se quer um X ou um O
+    quadroEscolha.addeventlistener( 'click', (event) => {
+        this.player =  event.target.classList.contains("X") ? "X" : "O";
+        return player;
+    });
 }
-play () {
+
+play (player) {
     espacos.forEach(espaco => { 
 
 espaco.addeventlistener("click", () => { //quando ele vai clicar no espaço vazio dos quadrados
     if (espaco !== 0) { //Se o espaço for diferente de null, pode colocar um elemento
-        
+        player =
     }
 })
 })
 }
+
 restart(matriz) { //Aqui vai transformar todos os números dentro da matriz em zero de novo. 
     for(i=0;i<3;i++) {
         for(j=0;j<3;j++) { 
-        matriz[i][j] = 0;
+        matriz[i][j] = "";
     }
+}
+}
+
+//Vai verificar se houve algum ganho
+verificarGanho(matriz,player) { //Aqui vou colocar as regras de como ganhar no jogo 
+    for(i=0;i<3;i++) {  //Se os valores da linha são preenchidos
+    if(matriz[i][0] === player && matriz[i][1] === player && matriz[i][2] === player)  {
+        return true;
+    }
+}
+
+for(j=0;j<3;j++) {// Se os valores das colunas foram preenchidos
+if(matriz[0][j] === player && matriz[1][j] === player && matriz[2][j] === player) {
+    return true;
+}
+}
+
+if(matriz[0][0] && matriz[1][1] && matriz[2][2] === player || matriz[2][0] && matriz[1][1] && matriz[0][2]) {
+    return true; //linha inclinada
+}
+}
+
+fimDeJogo(player){ // Vai verificar quem ganhou 
+    if(verificarGanho()) { 
+resultado = player === 'X' ? "Jogador X ganhou" : "Jogador O ganhou";
 }
 }
 }
@@ -33,9 +60,9 @@ restart(matriz) { //Aqui vai transformar todos os números dentro da matriz em z
 const espacos  = document.querySelectorAll(".espacos")
 
 const matriz = [ 
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
+    ["","",""],
+    ["","",""],
+    ["","",""]
 ] 
 
 
